b501efa1133009598b8e0e2a48a04cd63bf65ec4
Travis Johnson <travisj@gmail.com>
2011-05-30 12:18:11 -0400

    switiching how this works a bit, now you just tell it what to upload and it does it

--- bootstrap.php
+++ bootstrap.php
@@ -3,7 +3,9 @@
 Autoloader::add_core_namespace('S3');
 
 Autoloader::add_classes(array(
-	'S3\\Upload'					=> __DIR__.'/classes/upload.php',
+	'S3\\Upload'            => __DIR__.'/classes/upload.php',
+
+	// Observers
 ));
 
 
